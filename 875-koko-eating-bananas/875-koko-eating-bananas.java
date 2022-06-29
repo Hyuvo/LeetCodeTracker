b8350f370159f05c5e7db8f23574832184a8234d
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-06-29 02:18:08 -0400

    Time: 189 ms (5.08%), Space: 54.5 MB (15.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -5,14 +5,14 @@ class Solution {
         //as eating time monotonely decreases as eating speed increases
         //need to get the min speed under constraint of h hours
         //so search the left bound of eating speed(to get longer eating time)
-        int left = 1;
-        int right = 1000000000 + 1;
+        long left = 1;
+        long right = 1000000000;
         // for(int pile : piles) {
         //     if(pile > right) right = pile;
         // }
         
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            long mid = left + (right - left) / 2;
             if (eatingTime(piles, mid) == h) {
                 right = mid;
             } else if (eatingTime(piles, mid) < h) {
@@ -21,12 +21,12 @@ class Solution {
                 left = mid + 1;
             }
         }        
-        return left;
+        return (int) left;
     }
     
-    public int eatingTime(int[] piles, int speed) {
-        int time = 0;
-        for (int pile : piles) {
+    public long eatingTime(int[] piles, long speed) {
+        long time = 0;
+        for (long pile : piles) {
             time += pile / speed;
             if (pile % speed > 0) {
                 time++;
