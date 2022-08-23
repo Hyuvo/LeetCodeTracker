6b6f916494abd4af4027dccdf915465a7f175316
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-23 15:33:16 -0400

    Time: 20 ms (84.29%), Space: 45.8 MB (31.21%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,35 +1,60 @@
 class Solution {
-    // optimize with memo
-    private int[] memo;
-    
     public int coinChange(int[] coins, int amount) {
-        memo = new int[amount + 1];
-        Arrays.fill(memo, -111);
-        return dynamic(coins, amount);
-    }
-    
-    private int dynamic(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-        // if knows the result, no need to recalculate
-        if (memo[amount] != -111) {
-            return memo[amount];
-        }
+        // bottom up with dp table
         
-        int result = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            // subproblem is the # of coins
-            int subProblem = dynamic(coins, amount - coin);
-            if (subProblem == -1) {
-                continue;
+        // base case
+        // if (amount == 0) return 0;
+        // if (amount < 0) return -1;
+        
+        int[] dp = new int[amount + 1];
+        // the worst case is amount made up of all 1 coins
+        // so amount + 1 is the +infinity here
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; ++i) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
-            result = Math.min(result, subProblem + 1);
         }
-        // also store -1 when there's no solution
-        memo[amount] = (result == Integer.MAX_VALUE) ? -1 : result;
-        return memo[amount];
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
+
+// class Solution {
+//     // optimize with memo
+//     private int[] memo;
+    
+//     public int coinChange(int[] coins, int amount) {
+//         memo = new int[amount + 1];
+//         Arrays.fill(memo, -111);
+//         return dynamic(coins, amount);
+//     }
+    
+//     private int dynamic(int[] coins, int amount) {
+//         if (amount == 0) return 0;
+//         if (amount < 0) return -1;
+//         // if knows the result, no need to recalculate
+//         if (memo[amount] != -111) {
+//             return memo[amount];
+//         }
+        
+//         int result = Integer.MAX_VALUE;
+//         for (int coin : coins) {
+//             // subproblem is the # of coins
+//             int subProblem = dynamic(coins, amount - coin);
+//             if (subProblem == -1) {
+//                 continue;
+//             }
+//             result = Math.min(result, subProblem + 1);
+//         }
+//         // also store -1 when there's no solution
+//         memo[amount] = (result == Integer.MAX_VALUE) ? -1 : result;
+//         return memo[amount];
+//     }
+// }
 
 // class Solution {
 //     // brute force top-down
