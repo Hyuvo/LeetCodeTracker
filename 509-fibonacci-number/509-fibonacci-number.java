b832676ce86930bbcfd24629a21b6c606942593b
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-23 13:56:04 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (40.48%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,15 +1,29 @@
 class Solution {
-    // top down
     public int fib(int n) {
+        if (n == 0) return 0;
+        // bottom up
         int[] memo = new int[n + 1];
-        return helper(memo, n);
-    }
-    
-    private int helper(int[] memo, int n) {
-        if (n == 0 || n == 1) return n;
-        if (memo[n] != 0) return memo[n];
-        
-        memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
         return memo[n];
     }
 }
+
+// class Solution {
+//     // top down
+//     public int fib(int n) {
+//         int[] memo = new int[n + 1];
+//         return helper(memo, n);
+//     }
+    
+//     private int helper(int[] memo, int n) {
+//         if (n == 0 || n == 1) return n;
+//         if (memo[n] != 0) return memo[n];
+        
+//         memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+//         return memo[n];
+//     }
+// }
