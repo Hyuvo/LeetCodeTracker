af2a0e815b2be0d989e0c0fc071f67657c77f1b2
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-25 12:37:15 -0400

    Time: 17 ms (44.68%), Space: 49 MB (63.36%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -1,15 +1,18 @@
 class NumArray {
-    private int[] preSum;
+    int[] preSum;
     public NumArray(int[] nums) {
-        preSum = new int[nums.length + 1];
-        preSum[0] = 0;// init a head to add on
+        int n = nums.length;
+        // preSum[i] = sum of first i nums; preSum[2] = nums[0] + nums[1]
+        preSum = new int[n + 1];
         for (int i = 1; i < preSum.length; ++i) {
-            preSum[i] = preSum[i-1] + nums[i-1];
+            preSum[i] = preSum[i - 1] + nums[i - 1];
         }
     }
     
     public int sumRange(int left, int right) {
-        return preSum[right+1] - preSum[left];
+        // sum[left, right] inclusive
+        // sum(right + 1) - sum(left)
+        return preSum[right + 1] - preSum[left];
     }
 }
 
