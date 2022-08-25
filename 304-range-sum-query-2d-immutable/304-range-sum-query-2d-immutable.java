9f451e1b7603ed8065442d1427be8cfa0b5f151d
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-25 13:32:43 -0400

    Time: 203 ms (48.39%), Space: 126.8 MB (77.87%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,17 +1,21 @@
 class NumMatrix {
-    private int[][] preSum;
-    
+    int[][] preSum;
     public NumMatrix(int[][] matrix) {
-        preSum = new int[matrix.length + 1][matrix[0].length + 1];
-        for(int i = 1; i < matrix.length+1; ++i) {
-            for(int j = 1; j < matrix[0].length+1; ++j) {
-                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + matrix[i-1][j-1];
+        int m = matrix.length, n = matrix[0].length;
+        if (m == 0 || n == 0) return;
+        preSum = new int[m + 1][n + 1];        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                //preSum[i][j] is the sum of all nums within (0, 0) and (i - 1, j - 1)
+                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i- 1][j - 1] - preSum[i - 1][j - 1];
+                // count preSum[i - 1][j - 1] twice
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return preSum[row2+1][col2+1] - preSum[row2+1][col1] - preSum[row1][col2+1] + preSum[row1][col1];
+        // just draw it...
+        return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] - preSum[row1][col2 + 1] + preSum[row1][col1];
     }
 }
 
