c311273a94554d7264e09b1771ec0719979ea70a
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-25 12:13:23 -0400

    Time: 42 ms (70.17%), Space: 52.4 MB (40.51%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,23 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
+        // for each i, expand the palindrome from the center
         String result = "";
-        for(int i = 0; i < s.length(); ++i) {
+        for (int i = 0; i < s.length(); ++i) {
+            // s len is odd
             String s1 = palindrome(s, i, i);
-            String s2 = palindrome(s, i, i+1);
-            //longest(result, s1, s2)
+            // s len is even
+            String s2 = palindrome(s, i, i + 1);
+            // keep the longest
             result = result.length() > s1.length() ? result : s1;
             result = result.length() > s2.length() ? result : s2;
-        }
+        }     
         return result;
     }
     
     public String palindrome(String s, int l, int r) {
-        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                //expand from the center
-                l--;
-                r++;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                // when eligibile for palindrom,
+                // expand
+                --l;
+                ++r;
+                // the new border is not check
         }
-        // substr is [l, r) l, r have to be out of bound to stop iteration
+        // exclusive
         return s.substring(l + 1, r);
     }
 }
