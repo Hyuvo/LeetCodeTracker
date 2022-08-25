16ede3bb7208a084bf220d88dcccd221fce62707
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-25 11:44:56 -0400

    Time: 1 ms (35.27%), Space: 43 MB (12.34%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,11 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        if(nums.length == 0) return 0;
-        
-        int slow = 0, fast = 0;
-        
-        while(fast < nums.length) {
-            if(nums[fast] != val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
                 nums[slow++] = nums[fast];
             }
             ++fast;
