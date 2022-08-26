2908d91b92192533eee912ace94edb9168320471
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-26 17:45:49 -0400

    Time: 3 ms (61.82%), Space: 49 MB (86.24%) - LeetHub

--- 370-range-addition/370-range-addition.java
+++ 370-range-addition/370-range-addition.java
@@ -18,12 +18,15 @@ class Difference {
         diff = new int[n];
         diff[0] = nums[0];
         for (int i = 1; i < n; ++i) {
+            // diff[i] is the difference of nums[i] and nums[i - 1]
             diff[i] = nums[i] - nums[i - 1];
         }
     }
     
     public void increment(int start, int end, int val) {
         diff[start] += val;
+        // if the end is out of the bound of the array, means th modification applys till the end of the array
+        // then no need to deduct the val
         if (end + 1 < diff.length) {
             diff[end + 1] -= val;
         }
@@ -32,6 +35,7 @@ class Difference {
     public int[] result() {
         int[] result = new int[diff.length];
         result[0] = diff[0];
+        // by the definition of the diff array
         for (int i = 1; i < diff.length; ++i) {
             result[i] = result[i - 1] + diff[i];
         }
