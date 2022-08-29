806fb2747c2b0f6cb31df7ad7d6345e89ab680f9
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-29 14:12:25 -0400

    Time: 0 ms (100.00%), Space: 42.9 MB (38.83%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -2,13 +2,16 @@ class Solution {
     public void rotate(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         if (m == 0 || n == 0) return;
+        // first flip by the diangle
         for (int i = 0; i < m; ++i) {
+            // j start from i, cuz 0 ~ i - 1 have already been taken care of
             for (int j = i; j < n; ++j) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
+        // then reverse vertically;
         for (int[] row : matrix) {
             reverse(row);
         }
