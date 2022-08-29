e07ab7e031ef9c64143d744da034612cc6c508f4
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-29 14:49:53 -0400

    Time: 0 ms (100.00%), Space: 47 MB (74.47%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{leftBound(nums, target), rightBound(nums, target)};
+        return new int[]{searchLeft(nums, target), searchRight(nums, target)};
     }
     
-    public int leftBound(int[] nums, int target) {
+    public int searchLeft(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] == target) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
                 right = mid - 1;
-                // to approach left bound
-            } else if (nums[mid] < target) {
+            } else {
                 left = mid + 1;
-            } else if (nums[mid] > target) {
+            }
+        }
+        return (left >= nums.length || nums[left] != target) ? -1 : left;
+    }
+    
+    public int searchRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
                 right = mid - 1;
             }
         }
-        // check if out of bound
-        return (left >= nums.length || nums[left] != target)? -1 : left;
-    }
-    
-    public int rightBound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            if (nums[mid] == target) {
-                left = mid + 1;
-                // to approach left bound
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            }            
-        }
-        // check if out of bound
-        return (right < 0 || nums[right] != target)? -1 : right;
+        return (right < 0 || nums[right] != target) ? -1 : right;
     }
 }
