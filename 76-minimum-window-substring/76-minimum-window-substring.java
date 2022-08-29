10431b55a39b83188d79514a5c3e1de70c8e1122
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-08-29 19:39:40 -0400

    Time: 20 ms (60.54%), Space: 47 MB (45.20%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,55 +1,48 @@
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> window = new HashMap<>();
-        HashMap<Character, Integer> need = new HashMap<>();
-        for(int i = 0; i < t.length(); i++) {
-            // need[i]++
-            need.put(t.charAt(i), need.getOrDefault(t.charAt(i), 0) + 1);
+        HashMap<Character, Integer> target = new HashMap<>();
+        for (int i = 0; i < t.length(); ++i) {
+            char c = t.charAt(i);
+            target.put(c, target.getOrDefault(c, 0) + 1);
         }
-        // the window is [left, right)
-        int left = 0, right = 0, valid = 0; // valid is the # of qualified char in window
-        // start is the start index of the current min substring
-        // len is the current len of the current min substring
+        
+        // init window
+        int left = 0, right = 0, valid = 0;
+        // track the min substr
         int start = 0, len = Integer.MAX_VALUE;
-        while(right < s.length()) {
-            // the char that is going to be added to the window
+        
+        while (right < s.length()) {
+            // right exclusive window
             char curRight = s.charAt(right);
-            // expand the window
-            ++right;      
-            //update window data
-            if(need.containsKey(curRight)) {
-                //window[curRight]++
-                //window is to track #of needed char in current window
+            ++right;
+            if (target.containsKey(curRight)) {
                 window.put(curRight, window.getOrDefault(curRight, 0) + 1);
-                if(window.get(curRight).equals(need.get(curRight))){
-                    ++valid;
-                }
+                if (window.get(curRight).equals(target.get(curRight)))
+                    valid++;
             }
             
-            //if the window is qualified to shrink
-            while(valid == need.size()) {
-                // update the min substring
-                if(right - left < len) {
-                    // maintain the info of min substring
+            // left inclusive window
+            // maintian the window
+            // valid is the count of chars that meet the # requirement
+            while (valid == target.size()) {
+                // update if possible min substr
+                if (right - left < len) {
                     start = left;
                     len = right - left;
                 }
-                // the char that is gonna be removed from the window
-                char curLeft = s.charAt(left);
-                //shrink the window
-                ++left;
                 
-                // maintain the info of the window
-                if(need.containsKey(curLeft)) {
-                    // if window[curLeft] == need[curLeft]
-                    if(window.get(curLeft).equals(need.get(curLeft))) {
-                        --valid;                      
+                char curLeft = s.charAt(left);
+                ++left;
+                if (target.containsKey(curLeft)) {
+                    if (window.get(curLeft).equals(target.get(curLeft))) {
+                        --valid;
                     }
-                    //window[d]-- 
                     window.put(curLeft, window.get(curLeft) - 1);
                 }
             }
         }
-        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+        return len != Integer.MAX_VALUE ? s.substring(start, start + len) : "";
+            
     }
 }
