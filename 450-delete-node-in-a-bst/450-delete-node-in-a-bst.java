f1bc315b05d63e08a1b4bc73ee7c05f904ba0163
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-09-05 13:57:36 -0400

    Time: 0 ms (100.00%), Space: 49.8 MB (44.57%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -16,30 +16,38 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
-        if (root.val == key) {
-            // if the node has no child or only one child
-            if (root.left == null) return root.right;
-            if (root.right == null) return root.left;
-            
-            // if the node has 2 children
-            // find the max in left subtree or the min in the right subtree
-            // swap and delete
-            TreeNode successor = getSuccessor(root.right);
-            root.right = deleteNode(root.right, successor.val);
-            
-            successor.left = root.left;
-            successor.right = root.right;
-            root = successor;            
-        } else if (root.val < key) {
+        // search the node
+        if (root.val < key) {
             root.right = deleteNode(root.right, key);
-        } else {
+        } else if (root.val > key) {
             root.left = deleteNode(root.left, key);
+        } else if (root.val == key) {
+            // 1. the node is a leaf node
+            // 2. the node has only 1 child
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+                    
+            // 3. the node has both children
+            // swap with the min of right subtree  
+            TreeNode minNode = getMin(root.right);
+            root.right = deleteNode(root.right, minNode.val);
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
         }
         return root;
+            
+    }
+    public TreeNode getMin(TreeNode root) {
+        TreeNode p = root;
+        while(p.left != null) {
+            p = p.left;
+        }
+        return p;
     }
     
-    public TreeNode getSuccessor(TreeNode root) {
-        while (root.left != null) root = root.left;
-        return root;
-    }
 }
