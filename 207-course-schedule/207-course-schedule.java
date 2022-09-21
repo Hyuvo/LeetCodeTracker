639e25ac34d35cead50161f6387b90b67374f603
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-09-21 15:47:59 -0400

    Time: 9 ms (58.69%), Space: 47.4 MB (74.11%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,59 +1,52 @@
 class Solution {
-    private boolean[] visited;
-    private boolean[] onPath;
-    private boolean hasCycle = false;
+    // build a graph from prerequisites, and check if it has cycle
+    boolean hasCycle = false;
+    boolean[] visited;
+    // track current path
+    boolean[] onPath;
     
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // build graph, if exists circular dependency, cannot finish
-        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
-        
+        // node is 0-indexed
         visited = new boolean[numCourses];
         onPath = new boolean[numCourses];
+        LinkedList<Integer>[] graph = buildGraph(prerequisites, numCourses);
         
-        // not all nodes are connected, need to traverse from every node
+        // the graph may not be fully connected
+        // traverse from each node
         for (int i = 0; i < numCourses; ++i) {
-            // traversal
-            // see if cycle exists while traverse
             if (!visited[i]) traverse(graph, i);
-        }    
+        }
         
-        // if there's no cycle in the graph, it is possible to finish all courses
+        // if no cycle exixts, can finish all courses
         return !hasCycle;
-        
-        
     }
-    
-    public List<Integer>[] buildGraph(int numCourses, int[][] preReq) {
-        List<Integer>[] graph = new LinkedList[numCourses];
+    public void traverse(LinkedList<Integer>[] graph, int from) {
         
-        for(int i = 0; i < numCourses; ++i) {
+        if (onPath[from]) hasCycle = true;
+        // if found cycle already or visited
+        if (hasCycle || visited[from]) return;        
+        
+        visited[from] = true;
+        // traverse thru neighbors
+        // pre-order positon: set current node onPath
+        onPath[from] = true;
+        for (int to : graph[from]) {           
+            traverse(graph, to);            
+        }
+        // post-order: withdraw onPath selection
+        onPath[from] = false;
+    } 
+    
+    public LinkedList<Integer>[] buildGraph(int[][] edges, int n) {
+        LinkedList<Integer>[] graph = new LinkedList[n];
+        for (int i = 0; i < n; ++i) {
             graph[i] = new LinkedList<>();
         }
         
-        for(int[] pair : preReq) {
-            int from = pair[1], to = pair[0];
+        for (int[] edge : edges) {
+            int from = edge[1], to = edge[0];
             graph[from].add(to);
         }
-        
         return graph;
-    } 
-    
-    public void traverse(List<Integer>[] graph, int s) {
-        // if already on path, find cycle
-        if (onPath[s])  hasCycle = true;
-        
-        // use visited to prevent repeated computation
-        if (visited[s] || hasCycle) return;
-        
-        // preorder
-        visited[s] = true;
-        onPath[s] = true;
-        
-        for (int neighbor : graph[s]) {
-            traverse(graph, neighbor);
-        }
-        
-        // exit at postorder
-        onPath[s] = false;
     }
 }
