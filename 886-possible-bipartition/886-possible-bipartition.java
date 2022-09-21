5b11291f80b567413135c2e8342079b8955cfe68
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-09-21 14:19:03 -0400

    Time: 39 ms (45.81%), Space: 69.1 MB (76.08%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.java
+++ 886-possible-bipartition/886-possible-bipartition.java
@@ -1,56 +1,65 @@
 class Solution {
-    boolean[] visited, color;
+    // build a graph from dislikes, and see if bipartite
     boolean isBip = true;
+    boolean[] visited;
+    boolean[] color;
+    
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        List<Integer>[] graph = buildGraph(n, dislikes);       
-        // nodes start from 1
+        LinkedList<Integer>[] graph = buildGraph(dislikes, n);
+        // node is 1-indexed
         visited = new boolean[n + 1];
         color = new boolean[n + 1];
-        // subgraph may exist, need to start from each node
+        
+        // the graph may not be fully connected
+        // need to traverse from each node
         for (int i = 1; i <= n; ++i) {
-            if(!visited[i]) {
+            // if not traversed
+            if (!visited[i]) {
                 traverse(graph, i);
             }
         }
-        
         return isBip;
     }
     
-    public void traverse(List<Integer>[] graph, int start) {
-        // if is already not bipart, return
+    // traverse from start and dye
+    public void traverse(LinkedList<Integer>[] graph, int start) {
+        // if already know is not bip
         if (!isBip) return;
-        // dfs
+        // set visited true
         visited[start] = true;
-        // see its neighbors
+        // traverse thru neighbors
         for (int neighbor : graph[start]) {
+            // if not visited yet
             if (!visited[neighbor]) {
-                // if not visited, visit it and dye with the opposite color
+                // dye with opposit color and traverse
                 color[neighbor] = !color[start];
                 traverse(graph, neighbor);
-            } else{
-                // if visited, compare color
-                if (color[neighbor] == color[start]) {
+            } else { // if visited, compare colors
+                if (color[start] == color[neighbor]) {
+                    // the graph is not bip
                     isBip = false;
+                    return;
                 }
             }
         }
-        
     }
     
-    public List<Integer>[] buildGraph(int n, int[][] dislikes) {
-        // CAUTION: labeled from 1 to n
-        List<Integer>[] graph = new LinkedList[n + 1];
-        
+    public LinkedList<Integer>[] buildGraph (int[][] edges, int n) {
+        // CAUTION: node is 1-indexed
+        LinkedList<Integer>[] graph = new LinkedList[n + 1];
+        // open space
         for (int i = 1; i <= n; ++i) {
-            graph[i] = new LinkedList<>();
+            graph[i] = new LinkedList();
+        }
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            // CAUTION: bidirected
+            graph[a].add(b);
+            graph[b].add(a);
         }
         
-        for (int[] pair : dislikes) {
-            // CAUTION: undirected = bidirected
-            int v = pair[0], w = pair[1];
-            graph[v].add(w);
-            graph[w].add(v);           
-        }
         return graph;
     }
+    
+    
 }
