a1e990f9569bd86238a13194e86b7fe0803b72a0
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-09-23 15:33:04 -0400

    Time: 5 ms (71.28%), Space: 44.8 MB (80.96%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,70 +1,67 @@
 class Solution {
-    List<List<String>> result = new LinkedList<>();
+    private List<List<String>> result = new LinkedList();
     public List<List<String>> solveNQueens(int n) {
-        // store chess board with a char[][], representing the position of queen on each row
-        char[][] board = new char[n][n];
         // init an empty board
+        char[][] board = new char[n][n];
         for (char[] row : board) {
             Arrays.fill(row, '.');
         }
+        // start filling the board row by row
         backtrack(board, 0);
         return result;
     }
     
     public void backtrack(char[][] board, int row) {
-        // terminate when each row has a queen
-        // no -1
-        if (row == board.length) {
-            // complete one solution, add to the res
+        int n = board.length;
+        // see if reach the end row
+        if (row == n) {
+            // add the solution           
             result.add(charToString(board));
+            // finish a round
             return;
         }
-        int n = board[row].length;
-        // try each col on current row
-        for (int col = 0; col < n; ++col) {
-            // see if it would conflict with other queens
-            if (!isValid(board, row, col)) {
-                // if not eligible, skip
-                continue;
-            }
-            // make choice
-            board[row][col] = 'Q';
-            // recurse to next row
-            backtrack(board, row + 1);
-            // cancle choice
-            board[row][col] = '.';
-        }
         
+        for (int col = 0; col < n; ++col) {
+            // if not valid, skip
+            if (!isValid(board, row, col)) continue;
+            // make selection: place the Q
+            board[row][col] = 'Q';
+            // traverse to the next row
+            backtrack(board, row + 1);
+            // withdraw the selction
+            board[row][col] = '.';            
+        }
     }
     
     public boolean isValid(char[][] board, int row, int col) {
+        // check if conflict with completed rows and bound exceeding
         int n = board.length;
-        // check queen on 3 directions
-        
-        // 1. check each row on this col
-        for (int i = 0; i < n; ++i) {
-            if (board[i][col] == 'Q') return false;
+        // 1. check upside
+        for (int i = 0; i < row; ++i) {
+            if (board[i][col] == 'Q')
+                return false;
         }
-        // no need to check downwards
-        // 2. up-left
+        // 2. check up-left
+        // CAUTION: && 
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
-            if (board[i][j] == 'Q') return false;
+            if (board[i][j] == 'Q')
+                return false;
         }
-        // 3. up-right
+        // 3. check up-right
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
-            if (board[i][j] == 'Q') return false;
+            if (board[i][j] == 'Q')
+                return false;
         }
-            
-        return true;        
+        return true;
     }
     
     public List<String> charToString(char[][] board) {
-        // convert board to result list
-        List<String> solution = new LinkedList<>();
+        List<String> solution = new LinkedList();
         for (char[] row : board) {
             solution.add(new String(row));
         }
-        
         return solution;
     }
+    
+    
 }
