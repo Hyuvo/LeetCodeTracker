dea522a5c9ee7801c0ba7f35dddf5fcf192da153
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-06 16:17:04 -0400

    Time: 101 ms (59.18%), Space: 54.4 MB (19.63%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -1,63 +1,68 @@
 class Solution {
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        if (k > nums.length) return false;
+        // boolean[] used = new boolean[nums.length];
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
         if (sum % k != 0) return false;
         int target = sum / k;
+        // using bitmasking
         int used = 0;
-        
-        return backtrack(k, 0, nums, 0, target, used);            
+        return backtrack(k, 0, nums, 0, used, target);
     }
+    // use memo to track each tested num combination/arrangement
+    HashMap<Integer, Boolean> memo = new HashMap<>();
     
-    HashMap<Integer, Boolean> memo = new HashMap();
-    // put from bucket's pov, k buckets to be filled
-    public boolean backtrack(int k, int bucket, int[] nums, int start, int target, int used) {
+    public boolean backtrack(int k, int bucket, int[] nums, int start, int used, int target) {
         if (k == 0) {
-            // filled all buckets
+            // all bucket are assigned nums
+            // all nums must be assigned, as target = sum / k
             return true;
         }
         
-        // if complete the current bucket
         if (bucket == target) {
-            // move to the next bucket
-            boolean result = backtrack(k - 1, 0, nums, 0, target, used);
+            // the current bucket is completed
+            // fill next bucket
+            boolean result = backtrack(k - 1, 0, nums, 0, used, target);
+            // store the result
             memo.put(used, result);
-            return result;
         }
         
-        // if memoed this bucket filling plan
+        // look up the memo to avoid duplicate computation
         if (memo.containsKey(used)) {
             return memo.get(used);
         }
         
+        // fill with nums index from start
         for (int i = start; i < nums.length; ++i) {
-            // if the num is used[i]
-            // already in other bucket
+            // cut branches
             if (((used >> i) & 1) == 1) {
+                // if the ith num is used
                 continue;
             }
-            // current num cannot fit
+            // if exceed current bucket
             if (bucket + nums[i] > target) {
+                // got to next num
                 continue;
             }
             
-            // set current num used
+            // make a decision, put nums[i] into the bucket
+            // used[i] = true;
             used |= 1 << i;
-            // put current num in the bucket
             bucket += nums[i];
-            if (backtrack(k, bucket, nums, i + 1, target, used)) {
+            // see if next num can be put into current bucket
+            if (backtrack(k, bucket, nums, i + 1, used, target)) {
                 return true;
             }
-            // withdraw
+            
+            // withdraw the decision
+            // used[i] = false;
             used ^= 1 << i;
             bucket -= nums[i];
+            
             
         }
         return false;
     }
-    
-    
 }
