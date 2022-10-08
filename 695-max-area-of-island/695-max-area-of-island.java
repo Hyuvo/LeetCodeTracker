c009b465bec46deb1048a60f9dc324e889b98999
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-07 21:36:43 -0400

    Time: 4 ms (58.28%), Space: 47.2 MB (59.27%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,29 +1,32 @@
 class Solution {
-    // calculate the max area
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int maxArea = 0;
+        int max_area = 0;
+        // maintain max_area while traversing
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                maxArea = Math.max(maxArea, dfs(grid, i, j));            
+                max_area = Math.max(max_area, dfs(grid, i, j));
             }
         }
-        return maxArea;
+        return max_area;
     }
-    private int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // dfs return flooded area
     public int dfs(int[][] grid, int i, int j) {
-        // count the area while flooding the island
         int m = grid.length, n = grid[0].length;
-        // termination
-        // outta bound
-        if (i < 0 || i >= m || j < 0 || j >= n) return 0;
-        // already water
-        if (grid[i][j] == 0) return 0;
+        // validate (i, j)
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return 0;
+        }
+        if (grid[i][j] == 0) {
+            return 0;
+        }
         
-        // flood the island and its adjacent land
-        grid[i][j] = 0;
-        // track the area along the way
+        // flood and add up area
+        // start from current cell with area = 1
         int area = 1;
+        grid[i][j] = 0;
         for (int[] dir : directions) {
             area += dfs(grid, i + dir[0], j + dir[1]);
         }
