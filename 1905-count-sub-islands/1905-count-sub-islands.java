03790b3f0a0d255133d49d8d8e506538a40b5ea5
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-07 21:55:39 -0400

    Time: 30 ms (78.05%), Space: 74.3 MB (90.70%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,40 +1,40 @@
 class Solution {
-    
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         // when a land in grid2 is a grid of water in grid1, then it's not a sub-island
         int m = grid1.length, n = grid1[0].length;        
         // exclude all non-sub-islands first
-        for (int i = 0; i < m ; ++i) {
+        for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
+                // (i, j) cannot be a sub-island
                 if (grid2[i][j] == 1 && grid1[i][j] == 0) {
-                    // flood it!
+                    // flood it 
                     dfs(grid2, i, j);
                 }
             }
         }
-        // count survived sub-island
-        int count = 0;
-        for (int i = 0; i < m ; ++i) {
+        int num = 0;
+        // count lefted islands
+        for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid2[i][j] == 1) {
-                    // flood it!
-                    ++count;
+                    ++num;
                     dfs(grid2, i, j);
                 }
             }
         }
-        return count;
+        return num;
     }
-    int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     public void dfs(int[][] grid, int i, int j) {
         int m = grid.length, n = grid[0].length;
-        // termination
-        // outta bound
-        if (i < 0 || i >= m || j < 0 || j >= n) return;
-        // it's water
+        // validate(i, j)
+        if (i < 0 || i >= m || j < 0 || j >= n) {
+            return;
+        }
         if (grid[i][j] == 0) return;
         
-        // flood the island and its adjacent land
         grid[i][j] = 0;
         for (int[] dir : directions) {
             dfs(grid, i + dir[0], j + dir[1]);
