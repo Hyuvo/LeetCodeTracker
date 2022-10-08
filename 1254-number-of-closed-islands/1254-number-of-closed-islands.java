134fb9cbd1dc57eeedaac57d226fa651c72a1baf
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-07 21:02:35 -0400

    Time: 2 ms (96.67%), Space: 42.9 MB (90.96%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,46 +1,47 @@
 class Solution {
+    // borders of the graph is not considered as adjacent to lands
     public int closedIsland(int[][] grid) {
-        // flood every island on the borders
         int m = grid.length, n = grid[0].length;
-        // left and right borders
+        int num = 0;
+        // firstly flood 4 borders
         for (int i = 0; i < m; ++i) {
-            dfs(grid, i, 0);
-            dfs(grid, i, n - 1);            
+            // 1st col and last col
+                dfs(grid, i, 0);
+                dfs(grid, i, n - 1);
         }
         
-        // upper and lower borders
         for (int j = 0; j < n; ++j) {
-            dfs(grid, 0, j);
-            dfs(grid, m - 1, j);
+            // 1st row and last row
+                dfs(grid, 0, j);
+                dfs(grid, m - 1, j);
         }
-        
-        int count = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 0){
-                    count++;
+                if (grid[i][j] == 0) {
+                    ++num;
                     dfs(grid, i, j);
-                }                    
+                }
             }
         }
-        return count;
+        return num;
     }
-    int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     public void dfs(int[][] grid, int i, int j) {
-        // termination
         int m = grid.length, n = grid[0].length;
-        // outta bound
-        if (i < 0 || i >= m || j < 0 || j >= n) {
+        // vlidate (i, j)
+        if (i < 0 || i >=m || j < 0 || j >= n) {
             return;
         }
-        // is not island (not 0)
-        if (grid[i][j] == 1) {
-            return;
-        }        
-        // flood this island and its adjacent land
+        // if is water, skip
+        if (grid[i][j] == 1) return;
+        
+        // flood current land
         grid[i][j] = 1;
+        // flood its adjacent lands
         for (int[] dir : directions) {
             dfs(grid, i + dir[0], j + dir[1]);
         }
     }
+    
 }
