5f46656f8024ce59ad6c66799e94c04ee23ba2c1
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-12 16:32:41 -0400

    Time: 2 ms (34.08%), Space: 47.3 MB (83.11%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,24 @@
+class Solution {    
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        // base case
+        if (image[sr][sc] == color) return image;
+        dfs(image, sr, sc, image[sr][sc], color);
+        return image;
+        
+    }
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public void dfs(int[][] image, int i, int j, int old, int color) {
+        int m = image.length, n = image[0].length;
+        // validate (i, j)
+        if (i < 0 || i >= m || j < 0 || j >= n) return;
+        // color not match
+        if (image[i][j] != old) return;
+        // replace with new color
+        image[i][j] = color;
+        // traverse to neighbors
+        for (int[] dir : directions) {
+            dfs(image, i + dir[0], j + dir[1], old, color);
+        }
+            
+    }
+}
