0cdebbc9af39dd8c1a00dfd6015c8e92d5990746
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-13 17:04:36 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (70.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] memo;
+    public int climbStairs(int n) {
+        memo = new int[n + 1];
+        return dp(n);
+    }
+    
+    public int dp(int n) {
+        if (n == 1 || n == 2) return n;
+        if (memo[n] != 0) return memo[n];
+        
+        memo[n] = dp(n - 1) + dp(n - 2);
+        
+        return memo[n];
+    }
+}
