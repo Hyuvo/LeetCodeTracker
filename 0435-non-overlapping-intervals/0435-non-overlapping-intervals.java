fed17c137c3e8f16063c550dd939e3341172bcd2
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-15 00:05:10 -0400

    Time: 115 ms (45.61%), Space: 100.1 MB (68.35%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -5,6 +5,7 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
         int end = intervals[0][1];
         int count = 0;
+        // start from the 2nd interval
         for (int i = 1; i < n; ++i) {
             int start = intervals[i][0];
             // if overlaps, count
