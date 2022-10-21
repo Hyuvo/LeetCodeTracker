ebe124d8f20ddafa829cd2ebf4a38f4c93ce0f28
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-21 15:18:31 -0400

    Time: 2 ms (19.69%), Space: 41.2 MB (78.95%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,13 +1,7 @@
 class Solution {
     // K sum
     public List<List<Integer>> combinationSum3(int k, int n) {
-        // int[] nums = new int[9];
-        // for (int i = 1; i <= 9; ++i) {
-        //     nums[i - 1] = i;
-        // }
-        // return nSum(nums, k, n, 0);
-        return kSum(k, n, 1);
-        
+        return kSum(k, n, 1);        
     }
     
         public List<List<Integer>> kSum(int k, int target, int start) {
@@ -24,7 +18,8 @@ class Solution {
                 } else if (sum > target) {
                     right--;
                 } else {
-                    result.add(new LinkedList(List.of(left, right)));    
+                    result.add(new LinkedList(List.of(left, right)));   
+                    // Note: move left and right;
                     left++;
                     right--;
                 }
@@ -39,50 +34,10 @@ class Solution {
                     list.add(i);
                     result.add(list);
                 }
-            }
-            
+            }           
         }
         return result;
     }
-//     public List<List<Integer>> nSum(int[] nums, int n, long target, int start) {
-//         int size = nums.length;
-//         List<List<Integer>> result = new LinkedList<>();
-        
-//         // make sure at least 2 sum and array len should > n of sum
-//         if (n < 2 || size < n) return result;
-        
-//         // 2 sum as base case
-//         if (n == 2) {
-//             int low = start, high = size - 1;
-//             while (low < high) {
-//                 int left = nums[low], right = nums[high];
-//                 long sum = left + right;
-//                 if (sum < target) {
-//                     low++;
-//                 } else if (sum > target) {
-//                     high--;
-//                 } else {
-//                     // sum == target
-//                     result.add(new LinkedList<>(List.of(left, right)));
-//                     while (low < high && left == nums[low]) low++;
-//                     while (low < high && right == nums[high]) high--;
-//                 }  
-//             }          
-//             return result;
-//         } else {
-//             // n > 2
-//             for (int i = start; i < nums.length; ++i) {
-//                 List<List<Integer>> temp = nSum(nums, n - 1, target - nums[i], i + 1);
-//                 for (List<Integer> list : temp) {
-//                     list.add(nums[i]);
-//                     result.add(list);
-//                 }
-//                 while (i < size - 1 && nums[i] == nums[i + 1]) i++; 
-//             }
-//         }
-        
-//         return result;
-//     }
 }
     
     
