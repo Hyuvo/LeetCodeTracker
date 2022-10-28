086cf6e0355f9519c254c44ff3798108695e2dae
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-10-28 17:01:22 -0400

    Time: 67 ms (88.87%), Space: 118.6 MB (86.36%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,7 +1,65 @@
-// node will be store in a list and map through hash
+class LRUCache {
+    int cap;
+    HashMap<Integer, Node> map;
+    DoubleList cache;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+        map = new HashMap();
+        cache = new DoubleList();
+    }
+    
+    public void addRecent(int key, int val) {
+        Node x = new Node(key, val);
+        map.put(key, x);
+        cache.addLast(x);
+    }
+    
+    public void removeKey(int key) {
+        Node x = map.get(key);
+        map.remove(key);
+        cache.remove(x);
+    }
+    
+    public void makeRecent(int key) {
+        Node x = map.get(key);
+        cache.remove(x);
+        cache.addLast(x);
+    }
+    
+    public void removeLeastRecent() {
+        Node first = cache.removeFirst();
+        map.remove(first.key);
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        makeRecent(key);
+        return map.get(key).val;
+    }
+    
+    public void put(int key, int value) {
+        // if map contains key, update it 
+        if (map.containsKey(key)) {
+            removeKey(key);
+            addRecent(key, value);
+            // NOTE: return !!!!!
+            return;
+        }
+        // if no such key, check cap and put
+        // if cache is full, remove least recent
+        if (cap == cache.size()) {
+            removeLeastRecent();
+        }
+        addRecent(key, value);
+    }
+}
+
 class Node {
-    int key, val;
-    Node next, prev;
+    public int key, val;
+    public Node prev, next;
     public Node(int key, int val) {
         this.key = key;
         this.val = val;
@@ -9,35 +67,31 @@ class Node {
 }
 
 class DoubleList {
-    Node head, tail;
-    int size;
+    private int size;
+    private Node head, tail;
     public DoubleList() {
+        size = 0;
         // dummy head and tail
         head = new Node(0, 0);
         tail = new Node(0, 0);
         head.next = tail;
         tail.prev = head;
-        size = 0;
     }
-    
-    // regard head as least recent and tail as most recent
     
     public void addLast(Node node) {
         node.next = tail;
         node.prev = tail.prev;
         tail.prev.next = node;
-        tail.prev = node;
+        tail.prev = node;  
         size++;
     }
-    
-    // x must exist
+    // key must exist
     public void remove(Node x) {
         x.prev.next = x.next;
         x.next.prev = x.prev;
         size--;
     }
     
-    // why return first? to add to tail
     public Node removeFirst() {
         if (head.next == tail) {
             return null;
@@ -47,70 +101,10 @@ class DoubleList {
         return first;
     }
     
-    public int size() {
-        return size;
-    }
+    public int size() {return size;}
+    
 }
 
-class LRUCache {
-    int capacity;
-    DoubleList list;
-    HashMap<Integer, Node> keyToNode;
-    
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        list = new DoubleList();
-        keyToNode = new HashMap();
-    }
-    
-    // remove first and add to tail
-    public void makeRecent(int key) {
-        Node x = keyToNode.get(key);
-        list.remove(x);
-        list.addLast(x);
-    }
-    
-    public void addRecent(int key, int val) {
-        Node x = new Node(key, val);
-        keyToNode.put(key, x);
-        list.addLast(x);
-    }
-    
-    public void removeKey(int key) {
-        Node x = keyToNode.get(key);
-        keyToNode.remove(key);
-        list.remove(x);
-    }
-    
-    public void removeLeastRecent() {
-        Node x = list.removeFirst();
-        keyToNode.remove(x.key);
-    }
-    
-    public int get(int key) {
-        if (!keyToNode.containsKey(key)) {
-            return -1;
-        }
-        makeRecent(key);
-        return keyToNode.get(key).val;
-    }
-    
-    public void put(int key, int value) {
-        // if contains key, update
-        if (keyToNode.containsKey(key)) {
-            removeKey(key);
-            addRecent(key, value);
-            return;
-        }
-        // does not contain key
-        // if reach cap, remove least recent
-        if (list.size() == capacity) {
-            removeLeastRecent();   
-        }
-        addRecent(key, value);
-    }
-}
 
 /**
  * Your LRUCache object will be instantiated and called as such:
