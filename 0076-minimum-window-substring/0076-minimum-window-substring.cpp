269fcd93ab78e6315e3fe1c1efc02d7a867ba70d
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-04 15:38:47 -0400

    Time: 60 ms (24.55%), Space: 8.1 MB (30.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,41 +3,37 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char, int> need, window;
         for (char c : t) need[c]++;
-        // [left, right) as window
+        // window: [left, right)
         int left = 0, right = 0, valid = 0;
-        // track the min substr
+        // substring
         int start = 0, len = INT_MAX;
+        
         while (right < s.size()) {
-            // c is the char to be added into the window
-            char c = s[right];
-            right++;
-            // if need this char
+            // c is to be added into the window
+            char c = s[right++];
+            // maintain window
             if (need.count(c)) {
-                // update window
                 window[c]++;
-                if (window[c] == need[c]) {
+                if (need[c] == window[c]) {
                     valid++;
                 }
             }
-            
             // shrink left bound
             while (valid == need.size()) {
-                // maintian min substring
+                // update min substr
                 if (right - left < len) {
                     start = left;
                     len = right - left;
                 }
-                // d is the char to be removed from the window
-                char d = s[left];
-                left++;
-                if (need.count(d)) {                    
+                // d is to be removed from the left
+                char d = s[left++];                
+                if (need.count(d)) {
                     if (need[d] == window[d]) {
-                        valid--;                       
+                        valid--;
                     }
-                    // deduct after validate
                     window[d]--;
-                }            
-            }
+                }
+            }           
         }
         return len == INT_MAX ? "" : s.substr(start, len);
     }
