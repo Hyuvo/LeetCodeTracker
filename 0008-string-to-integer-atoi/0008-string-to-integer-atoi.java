7d208eda1a3736596e9d31aabfb37c15485ea535
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-03 21:09:01 -0400

    Time: 3 ms (80.53%), Space: 42.9 MB (60.96%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,36 +1,36 @@
 class Solution {
     public int myAtoi(String s) {
+        // default sign +
         int sign = 1;
         int result = 0;
         int index = 0;
         int n = s.length();
         
-        // ignore leading whitespace
-        while (index < n && s.charAt(index) == ' ') {
-            index++;
-        }
+        // 1. ignore leading whitespace
+        while (index < n && s.charAt(index) == ' ') index++;
         
+        // 2. check the sign
         if (index < n && s.charAt(index) == '-') {
             sign = -1;
             index++;
         } else if (index < n && s.charAt(index) == '+') {
-            // sign = 1;
             index++;
         }
         
-        // read the number
+        // 3. read digit
         while (index < n && Character.isDigit(s.charAt(index))) {
             int digit = s.charAt(index) - '0';
-            // check overflow for current result before add           
-            // clamp
+            // check overflow first
             if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10) {
-                // check the sign
+                // return per sign
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             
+            // append to result
             result = result * 10 + digit;
+            // don't forget index
             index++;
         }
-        return sign * result;        
+        return result * sign;
     }
 }
