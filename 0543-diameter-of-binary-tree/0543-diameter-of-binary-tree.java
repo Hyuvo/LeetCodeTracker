dd69459a998cb3ac3b9df7acf3fa7f56d75df57a
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-05 14:51:56 -0400

    Time: 0 ms (100.00%), Space: 42.3 MB (83.07%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxDiam = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiam;
+    }
+    
+    // return the depth of a tree
+    public int maxDepth(TreeNode root) {
+        // base case;
+        if (root == null) return 0;
+        // diameter = left maxD + right maxD             
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        // post-order: maintain max diameter
+        maxDiam = Math.max(maxDiam, leftMax + rightMax);
+        // depth = the depth of the deeper subtree
+        return Math.max(leftMax, rightMax) + 1;
+    }
+}
