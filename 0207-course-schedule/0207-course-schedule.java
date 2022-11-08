6e0de72d7777414beee53ccccbc47dc961b2a958
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-08 14:28:36 -0500

    Time: 10 ms (67.99%), Space: 48.3 MB (47.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,48 @@
 class Solution {
-    // build graph per prerequisites, and check if has cycle
-    private boolean[] onPath;
-    private boolean[] visited;
-    private boolean hasCycle = false;
+    // build a graph from prerequisites, and check if it has cycle
+    boolean hasCycle = false;
+    boolean[] visited;
+    // track current path
+    boolean[] onPath;
     
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
-        onPath = new boolean[numCourses];
+        // node is 0-indexed
         visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+        LinkedList<Integer>[] graph = buildGraph(prerequisites, numCourses);
+        
+        // the graph may not be fully connected
+        // traverse from each node
         for (int i = 0; i < numCourses; ++i) {
             traverse(graph, i);
         }
+        
+        // if no cycle exixts, can finish all courses
         return !hasCycle;
     }
-    
-    public void traverse(List<Integer>[] graph, int from) {
+    public void traverse(LinkedList<Integer>[] graph, int from) {
+        
         if (onPath[from]) hasCycle = true;
-        if (visited[from] || hasCycle) return;
+        // if found cycle already or visited
+        if (hasCycle || visited[from]) return;        
         
-        // pre-order
-        onPath[from] = true;
         visited[from] = true;
-        
-        for (int neighbor : graph[from]) {
-            traverse(graph, neighbor);
+        // traverse thru neighbors
+        // pre-order positon: set current node onPath
+        onPath[from] = true;
+        for (int to : graph[from]) {           
+            traverse(graph, to);            
         }
+        // post-order: withdraw onPath selection
         onPath[from] = false;
-    }
+    } 
     
-    private List<Integer>[] buildGraph(int num, int[][] edges) {
-        List<Integer>[] graph = new LinkedList[num];
-        for (int i = 0; i < num; ++i) {
-            graph[i] = new LinkedList<Integer>();
+    public LinkedList<Integer>[] buildGraph(int[][] edges, int n) {
+        LinkedList<Integer>[] graph = new LinkedList[n];
+        for (int i = 0; i < n; ++i) {
+            graph[i] = new LinkedList<>();
         }
+        
         for (int[] edge : edges) {
             int from = edge[1], to = edge[0];
             graph[from].add(to);
