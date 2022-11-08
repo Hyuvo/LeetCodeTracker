45eba5dda560ae39db20822017519b2e4158c0d3
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-07 22:35:49 -0500

    Time: 316 ms (77.66%), Space: 217 MB (63.05%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,24 +1,57 @@
 class TimeMap {
-    private HashMap<String, TreeMap<Integer, String>> keyTimeMap;
+    private HashMap<String, ArrayList<Pair<Integer, String>>> keyTimeMap;
     public TimeMap() {
-        // treemap store timestamp ascendingly
-        keyTimeMap = new HashMap<String, TreeMap<Integer, String>>();
+        keyTimeMap = new HashMap();
     }
-    
+    // All the timestamps timestamp of set are strictly increasing.
     public void set(String key, String value, int timestamp) {
         if (!keyTimeMap.containsKey(key)) {
-            keyTimeMap.put(key, new TreeMap<Integer, String>());
+            keyTimeMap.put(key, new ArrayList<Pair<Integer, String>>());
         }
-        keyTimeMap.get(key).put(timestamp, value);
+        keyTimeMap.get(key).add(new Pair(timestamp, value));
     }
     
     public String get(String key, int timestamp) {
         if (!keyTimeMap.containsKey(key)) return "";
-        Integer floorKey = keyTimeMap.get(key).floorKey(timestamp);
+        // if timestamp is smaller than the min timestamp stored
+        if (keyTimeMap.get(key).get(0).getKey() > timestamp) return "";
         
-        return floorKey == null ? "" : keyTimeMap.get(key).get(floorKey);
+        // binary search right bound
+        int left = 0, right = keyTimeMap.get(key).size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (keyTimeMap.get(key).get(mid).getKey() <= timestamp) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (right == -1) return "";
+        return keyTimeMap.get(key).get(right).getValue();
     }
 }
+
+// class TimeMap {
+//     private HashMap<String, TreeMap<Integer, String>> keyTimeMap;
+//     public TimeMap() {
+//         // treemap store timestamp ascendingly
+//         keyTimeMap = new HashMap<String, TreeMap<Integer, String>>();
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         if (!keyTimeMap.containsKey(key)) {
+//             keyTimeMap.put(key, new TreeMap<Integer, String>());
+//         }
+//         keyTimeMap.get(key).put(timestamp, value);
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         if (!keyTimeMap.containsKey(key)) return "";
+//         Integer floorKey = keyTimeMap.get(key).floorKey(timestamp);
+        
+//         return floorKey == null ? "" : keyTimeMap.get(key).get(floorKey);
+//     }
+// }
 
 /**
  * Your TimeMap object will be instantiated and called as such:
