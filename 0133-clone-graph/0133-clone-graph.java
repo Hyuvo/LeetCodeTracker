d3447299b2ee53c0572233ea41ea3743883e6529
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-08 13:54:19 -0500

    Time: 59 ms (6.45%), Space: 42.6 MB (81.69%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private HashMap<Node, Node> oriToClone = new HashMap();
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        traverse(node);
+        return oriToClone.get(node);
+    }
+    
+    // dfs
+    public void traverse(Node node) {
+        if (node == null) return;
+        if (oriToClone.containsKey(node)) return;
+        oriToClone.put(node, new Node(node.val));
+        
+        for (Node neighbor : node.neighbors) {
+            traverse(neighbor);
+            oriToClone.get(node).neighbors.add(oriToClone.get(neighbor));
+        }
+    }
+}
