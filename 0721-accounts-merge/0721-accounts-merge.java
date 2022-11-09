64c7d58cca6c036377130b4b5ae2f4659b3bb94b
Hyuvo <32900542+Hyuvo@users.noreply.github.com>
2022-11-09 17:14:56 -0500

    Time: 74 ms (57.63%), Space: 63.1 MB (52.89%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -2,41 +2,40 @@ class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
         UnionFind uf = new UnionFind(n);
-        // account id from 0 to n - 1
         HashMap<String, Integer> emailToId = new HashMap();
-        
-        // build union find and merge accounts per email
+        // email => account id, merge accounts if have same email
         for (int i = 0; i < n; ++i) {
             int size = accounts.get(i).size();
             for (int j = 1; j < size; ++j) {
-                String mail = accounts.get(i).get(j);
-                if (!emailToId.containsKey(mail)) {
-                    // map a mail to its original account
-                    emailToId.put(mail, i);
+                String email = accounts.get(i).get(j);
+                if (!emailToId.containsKey(email)) {
+                    emailToId.put(email, i);
                 } else {
-                    // if seen the email before, merge 2 accounts
-                    uf.union(i, emailToId.get(mail));
+                    uf.union(i, emailToId.get(email));
                 }
             }
         }
+        // account => id, merge if have same uf root
         HashMap<Integer, List<String>> idToEmails = new HashMap();
-        // map emails within the same union to the same account
         for (Map.Entry<String, Integer> entry : emailToId.entrySet()) {
             // find root account id
             int id = uf.find(entry.getValue());
-            // merge emails with the same id
+            // merge emails
             List<String> emails = idToEmails.getOrDefault(id, new ArrayList<String>());
             emails.add(entry.getKey());
             idToEmails.put(id, emails);
         }
-        // map username to email
+        // combine username and sorted emails
         List<List<String>> result = new ArrayList();
         for (Map.Entry<Integer, List<String>> entry : idToEmails.entrySet()) {
+            int id = entry.getKey();
+            // get username per id from accounts
+            String name = accounts.get(id).get(0);
             List<String> emails = entry.getValue();
             Collections.sort(emails);
-            // add username (get from account id)
+            
             List<String> temp = new ArrayList<>();
-            temp.add(accounts.get(entry.getKey()).get(0));
+            temp.add(name);
             temp.addAll(emails);
             result.add(temp);
         }
@@ -45,7 +44,7 @@ class Solution {
 }
 
 class UnionFind {
-    private int[] parent;
+    int[] parent;
     
     public UnionFind(int n) {
         parent = new int[n];
@@ -53,22 +52,12 @@ class UnionFind {
             parent[i] = i;
         }
     }
-    
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
-        
-        if (rootP == rootQ)
-            return;
+        if (rootP == rootQ) return;
         parent[rootQ] = rootP;
-        
     }
-    // public boolean isConnected(int p, int q) {
-    //     int rootP = find(p);
-    //     int rootQ = find(q);
-    //     return rootP == rootQ;
-    // }
-    // find root
     public int find(int x) {
         if (parent[x] != x) {
             parent[x] = find(parent[x]);
